1d918605e0dc658857b288aa6f6af641c11dcbb1
zwchao <zwchao95@outlook.com>
2017-05-14 14:36:56 +0800

    爱驻夕阳网站

---
+++ FE-demo/psyweb/scripts/psy.js
@@ -0,0 +1,10 @@
+$(function(){
+    var posterTvGrid = new posterTvGrid('posterTvGrid',{className: "posterTvGrid"},[
+        {"img":"imgs\/1.jpg","title":"students","url":"#"},
+        {"img":"imgs\/2.jpg","title":"students","url":"#"},
+        {"img":"imgs\/3.jpg","title":"students","url":"#"},
+        {"img":"imgs\/4.jpg","title":"students","url":"#"},
+        {"img":"imgs\/5.jpg","title":"students","url":"#"},
+        {"img":"imgs\/6.jpg","title":"students","url":"#"}       
+    ]);
+})
---
+++ JQuery/website/scripts/main.js
@@ -0,0 +1,24 @@
+requirejs.config({
+    paths:{
+        jquery: 'jquery-3.2.0.min'
+    }
+});
+requirejs(['jquery'],function($){
+    
+    $("inputSearch").focus(function(){
+        $(this).addClass("focus");
+        if($(this).val()===this.defaultValue){
+            $(this).val("");
+        }
+    }).blur(function(){
+        $(this).removeClass("focus");
+        if($(this).val()===''){
+            $(this).val()=this.defaultValue;
+        }
+    }).keyup(function(e){
+        if(e.which == 13){
+            alert('回车提交表单');
+        }
+    })
+
+});
